368fcadd66beb4bee3f712f8946e50cf81f5f755
calvinchankf <chan9118kin@gmail.com>
2018-06-09 18:39:38 +0800

    try defer

--- main.go
+++ main.go
@@ -6,15 +6,7 @@ import (
 	"github.com/calvinchankf/try-to-go/cmd"
 )
 
-func add(a int, b int) int {
-	c := a + b
-	return c
-}
-
 func main() {
 	fmt.Println("hello world")
-	result := add(1, 2)
-	fmt.Printf("%d", result)
-
 	cmd.Execute()
 }
