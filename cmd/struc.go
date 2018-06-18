71ebfb31173f18c2d6bbb36f6d1218dbcf6aca51
calvinchankf <chan9118kin@gmail.com>
2018-06-18 15:18:51 +0800

    i hate go

--- cmd/struc.go
+++ cmd/struc.go
@@ -11,6 +11,18 @@ var strucCmd = &cobra.Command{
 	Short: "A brief description of your command",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("struc called")
+
+		var ptr *int
+		if ptr != nil {
+			fmt.Println("has")
+		} else {
+			fmt.Println("has not")
+		}
+
+		i := 1
+		ptr = &i
+		fmt.Println(*ptr)
+
 		struc()
 	},
 }
@@ -22,32 +34,68 @@ func init() {
 // class
 
 type Car struct {
-	name  string
-	price float64
+	name  *string
+	price *float64
 }
 
 type Ferrari struct {
 	Car
-	formula int
+	formula *int
 }
 
-func (c Car) Name() string {
+func (c Car) Name() *string {
 	return c.name
 }
 
-func (c Car) Price() float64 {
+func (c Car) Price() *float64 {
 	return c.price
 }
 
-func (f Ferrari) Formula() int {
+func (f Ferrari) Formula() *int {
 	return f.formula
 }
 
 // try construct
 
 func struc() {
-	ferrari := Ferrari{Car{"ferra", 1000000}, 1}
-	fmt.Println("ff", ferrari.name)
+	// a := "calvin"
+	// var a string
+	// a_ptr := &a
+
+	ferrari := Ferrari{
+		Car{
+			returnStringAdd("calvin"),
+			returnFloatAdd(1),
+		},
+		returnIntAdd(2),
+	}
+	fmt.Println("ff", *ferrari.name)
 	fmt.Println("ff", ferrari.price)
 	fmt.Println("ff", ferrari.formula)
+
+	benzi := Ferrari{
+		Car{
+			nil,
+			nil,
+		},
+		returnIntAdd(2),
+	}
+	fmt.Println("ff", benzi.name)
+	fmt.Println("ff", benzi.price)
+	fmt.Println("ff", benzi.formula)
+}
+
+func returnStringAdd(value string) *string {
+	a := value
+	return &a
+}
+
+func returnFloatAdd(value float64) *float64 {
+	a := value
+	return &a
+}
+
+func returnIntAdd(value int) *int {
+	a := value
+	return &a
 }
